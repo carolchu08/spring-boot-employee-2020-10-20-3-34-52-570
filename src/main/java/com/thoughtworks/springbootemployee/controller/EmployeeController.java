4de2d903114c67af8d738569e2dd53870c2a794a
CAROL CHU (OLP-ISD-OOCLL/HKG) <CHUCA2@OOCL.COM>
2020-12-02 18:00:23 +0800

    feat: get all employee with page and pagesize

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -23,6 +23,35 @@ public class EmployeeController {
         return employees.stream().filter(employees->employees.getEmployeeID().equals(employeeID)).findFirst().orElse(null);
 
     }
+    @GetMapping(params={"page","pageSize"})
+    public List<Employees> getAllEmployee(@RequestParam int page, @RequestParam int pageSize){
+        //employees.add(new Employees("pater","123",12,1000,"male"));
+        return getPage(employees,page,pageSize);
+    }
+
+    /**
+     * returns a view (not a new list) of the sourceList for the
+     * range based on page and pageSize
+     * @param sourceList
+     * @param page, page number should start from 1
+     * @param pageSize
+     * @return
+     * custom error can be given instead of returning emptyList
+     */
+    public static <T> List<T> getPage(List<T> sourceList, int page, int pageSize) {
+        if(pageSize <= 0 || page <= 0) {
+            throw new IllegalArgumentException("invalid page size: " + pageSize);
+        }
+
+        int fromIndex = (page - 1) * pageSize;
+        if(sourceList == null || sourceList.size() <= fromIndex){
+            return Collections.emptyList();
+        }
+
+        // toIndex exclusive
+        return sourceList.subList(fromIndex, Math.min(fromIndex + pageSize, sourceList.size()));
+    }
+
 
 
 }
