132aa6c34e1ba32ea1d854c9524a2394763e680a
CAROL CHU (OLP-ISD-OOCLL/HKG) <CHUCA2@OOCL.COM>
2020-12-03 23:38:24 +0800

    feat :can return all company

--- src/test/java/com/thoughtworks/springbootemployee/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyIntegrationTest.java
@@ -1,4 +1,49 @@
 package com.thoughtworks.springbootemployee;
 
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository1;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class CompanyIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    CompanyRepository1 companyRepository1;
+    @AfterEach
+    void tearDown(){
+        employeeRepository.deleteAll();
+    }
+    @Test
+    public void should_return_all_company_when_getAll_given_all_company() throws Exception{
+        //given
+        companyRepository1.save(new Company("companyA","1",20));
+
+
+        //when
+        // then
+        mockMvc.perform(get("/company"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].companyName").value("companyA"))
+                .andExpect(jsonPath("$[0].companyID").value("1"))
+                .andExpect(jsonPath("$[0].employeeNum").value(20));
+
+
+
+    }
+
 }
--- src/test/java/com/thoughtworks/springbootemployee/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeIntegrationTest.java
@@ -1,22 +1,14 @@
 package com.thoughtworks.springbootemployee;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
-import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
